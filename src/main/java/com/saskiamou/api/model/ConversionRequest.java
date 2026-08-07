440b91f7ba198e11064750b2bdb565e857a4a970
saskiamou <jade.saskiamou@gmail.com>
2026-08-06 22:11:41 -0500

    import lombok - Getter/Setter annotations(remove boilerplate)

--- src/main/java/com/saskiamou/api/model/ConversionRequest.java
+++ src/main/java/com/saskiamou/api/model/ConversionRequest.java
@@ -1,37 +1,20 @@
 package com.saskiamou.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ConversionRequest {
     private double amount;
     private String firstCurrency;
     private String secondCurrency;
 
-    public ConversionRequest() {
-    }
 
     public ConversionRequest(double amount, String firstCurrency, String secondCurrency) {
         this.amount = amount;
         this.firstCurrency = firstCurrency;
         this.secondCurrency = secondCurrency;
     }
-
-    public double getAmount() {
-        return amount;
-    }
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-    public String getFirstCurrency() {
-        return firstCurrency;
-    }
-    public void setFirstCurrency(String firstCurrency) {
-        this.firstCurrency = firstCurrency;
-    }
-    public String getSecondCurrency() {
-        return secondCurrency;
-    }
-    public void setSecondCurrency(String secondCurrency) {
-        this.secondCurrency= secondCurrency;
-    }
-
 
 }
