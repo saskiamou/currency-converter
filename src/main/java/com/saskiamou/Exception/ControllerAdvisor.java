6310d4eca5f264482563e4e31d6b4d651fc5ff39
saskiamou <jade.saskiamou@gmail.com>
2026-08-07 14:29:03 -0500

    Implement @ControllerAdvice for UnknownCurrencyException, fix formatting

--- src/main/java/com/saskiamou/Exception/ControllerAdvisor.java
+++ src/main/java/com/saskiamou/Exception/ControllerAdvisor.java
@@ -1,18 +1,19 @@
 package com.saskiamou.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
 
 @ControllerAdvice
-
-public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+public class ControllerAdvisor {
 
     @ExceptionHandler(UnknownCurrencyException.class)
-    public ResponseEntity<Object> handleUnknownCurrencyException{
-        UnknownCurrencyException e, WebRequest r) {
-
-        }
+    public ResponseEntity<String>
+    handleUnknownCurrencyException(UnknownCurrencyException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/com/saskiamou/Exception/UnknownCurrencyException.java
+++ src/main/java/com/saskiamou/Exception/UnknownCurrencyException.java
@@ -3,6 +3,6 @@ package com.saskiamou.Exception;
 public class UnknownCurrencyException extends RuntimeException {
 
     public UnknownCurrencyException(String from, String to) {
-      super(String.format("Unknown currency: %s, %s", to, from));
+      super(String.format("Unknown currency - from: %s, to: %s", from, to));
     }
 }
